fe21b6ad46a462ae1d4ec9ac6369e2486fb29f88
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-23 15:56:16 +0000

    Ex07 Solved

---
+++ Lucas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a int");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Lucas de {n} = {Lucas(n)}");
+        }
+        private static int Lucas(int n)
+        {
+            if (n == 0) return 2;
+            if (n == 1) return 1;
+            return Lucas(n - 1) + Lucas(n - 2);
+        }
+    }
+}
