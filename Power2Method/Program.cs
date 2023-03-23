0db14fcd3ff29533c7a3104666eac7c40cd0de88
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-23 14:53:53 +0000

    Ex01 Solved

---
+++ Power2Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowersOf2Until5();//Method call
+            PowersOf2Until5();//Method call
+        }
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (i << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
