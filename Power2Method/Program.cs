d5c6b112308e84569f948a0d1b5a41412d86f261
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-23 15:12:45 +0000

    Ex04 Soved

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -6,17 +6,18 @@ namespace Power2Method
     {
         static void Main(string[] args)
         {
-            string aux = Console.Readline();
+            string aux = Console.ReadLine();
             int n = Convert.ToInt32(aux);
+
 
             PowersOf2UntilN(2);//Method call
             PowersOf2UntilN(5);//Method call
         }
         private static void PowersOf2Until5()
         {
-            for (int i = 1; i <= (i << 5); i = i << 1)
+            for (int i = 1; i <= 32; i = i << 1)
             {
-                Console.WriteLine(i);
+                PowersOf2UntilN();
             }
         }
         private static void PowersOf2UntilN(int n)
