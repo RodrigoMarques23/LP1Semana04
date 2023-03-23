0b9466403c0aff2c95cebc66bff51ef0a28e7fb9
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-23 15:21:46 +0000

    Ex05 Solved

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,15 +4,21 @@ namespace Power2Method
 {
     class Program
     {
+        /// <summary>
+        /// reads a string given by user
+        /// convert the string into a int
+        /// call PowersOf2Until5 method
+        /// </summary>
+        /// <param name="args"></param> args passed by user
         static void Main(string[] args)
         {
             string aux = Console.ReadLine();
             int n = Convert.ToInt32(aux);
-
-
-            PowersOf2UntilN(2);//Method call
-            PowersOf2UntilN(5);//Method call
+            PowersOf2Until5();
         }
+        /// <summary>
+        /// call PowerOf2UntilN till i reachs 32
+        /// </summary>
         private static void PowersOf2Until5()
         {
             for (int i = 1; i <= 32; i = i << 1)
@@ -20,6 +26,10 @@ namespace Power2Method
                 PowersOf2UntilN();
             }
         }
+        /// <summary>
+        /// prints i unit i reachs the max value defined by user
+        /// </summary>
+        /// <param name="n"></param> int given by user
         private static void PowersOf2UntilN(int n)
         {
             for (int i = 1; i <= (i << n); i = i << 1)
